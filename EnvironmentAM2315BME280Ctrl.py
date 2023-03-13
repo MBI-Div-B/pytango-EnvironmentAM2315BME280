96380da1bdfdd05f2cc079577d1ea0ad8e190872
Daniel Schick <schick@mbi-berlin.de>
2023-03-13 14:20:48 +0100

    fix renaming and flake8

--- EnvironmentAM2315BME280Ctrl.py
+++ EnvironmentAM2315BME280Ctrl.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3 -u
 # coding: utf8
-# PhytronMCC2Ctrl
-from tango import DevState, AttrWriteType, DispLevel, DebugIt
-from tango.server import Device, attribute, command, device_property
+# EnvironmentAM2315BME280Ctrl
+from tango import DevState, DebugIt
+from tango.server import Device, command, device_property
 import AM2315
 import board
 from adafruit_bme280 import basic as adafruit_bme280
-import time 
 
-class AM2315BME280MuxCtrl(Device):
+
+class EnvironmentAM2315BME280Ctrl(Device):
     # device properties
     MuxAddress = device_property(
         dtype=int,
@@ -20,15 +20,14 @@ class AM2315BME280MuxCtrl(Device):
         self.set_state(DevState.INIT)
         self._am1 = AM2315.AM2315(1)
         self.i2c = board.I2C()
-        self.sensor_types = {0: "AM2315" ,
-                             1: "BME280" }
+        self.sensor_types = {0: "AM2315",
+                             1: "BME280"}
         self.bme_init = False
         self.set_state(DevState.ON)
         self._current_channel = None
-        
-    @DebugIt()
+
     @command(dtype_in=(int,), dtype_out=str)
-    def init_sensor(self,channel_sens):
+    def init_sensor(self, channel_sens):
         error = ""
         sens_type = self.sensor_types[channel_sens[1]]
         if sens_type == "AM2315" or (sens_type == "BME280" and self.bme_init):
@@ -39,23 +38,23 @@ class AM2315BME280MuxCtrl(Device):
                 self._bme1 = adafruit_bme280.Adafruit_BME280_I2C(self.i2c)
                 self.bme_init = True
             except ValueError:
-                error = "BME280 not connected to Channel " + str(channel)
+                error = "BME280 not connected to Channel " + str(channel_sens)
         else:
             error = "wrong Sensor type/name"
         return error
-    
-    @DebugIt()
-    @command(dtype_in=(int,) , dtype_out=(float,))
+
+    @command(dtype_in=(int,), dtype_out=(float,))
     def read_data(self, channel_sens):
         sens_type = self.sensor_types[channel_sens[1]]
-        if channel_sens[0] != self._current_channel :
+        if channel_sens[0] != self._current_channel:
             self.mux_select(channel_sens[0])
-            self._current_channel=channel_sens[0]
+            self._current_channel = channel_sens[0]
         try:
-            if sens_type == "AM2315": #AM2315
+            if sens_type == "AM2315":  # AM2315
                 data = (self._am1.read_temperature(), self._am1.humidity)
-            elif sens_type == "BME280":#BME280
-                data = (self._bme1.temperature,self._bme1.humidity,self._bme1.pressure)
+            elif sens_type == "BME280":  # BME280
+                data = (self._bme1.temperature, self._bme1.humidity,
+                        self._bme1.pressure)
             else:
                 self.error_stream('could not read Device. wrong device')
         except Exception:
@@ -63,7 +62,6 @@ class AM2315BME280MuxCtrl(Device):
             data = (-1.)
         return data
 
-        
     @DebugIt()
     def mux_select(self, channel):
         if channel > 7:
@@ -71,5 +69,6 @@ class AM2315BME280MuxCtrl(Device):
             return
         self.i2c.writeto(self.MuxAddress, bytearray([1 << channel]))
 
+
 if __name__ == "__main__":
-    AM2315BME280MuxCtrl.run_server()
+    EnvironmentAM2315BME280Ctrl.run_server()
--- EnvironmentAM2315BME280Sensor.py
+++ EnvironmentAM2315BME280Sensor.py
@@ -7,27 +7,29 @@
 # Author: Luca Barbera / Email: barbera@mbi-berlin.de
 
 
-from tango import AttrWriteType, DevState, DebugIt, ErrorIt, InfoIt, DeviceProxy, DevFailed, ConnectionFailed
-from tango.server import Device, attribute, command, device_property
+from tango import AttrWriteType, DevState, DeviceProxy, DevFailed
+from tango import ConnectionFailed
+from tango.server import Device, attribute, device_property
 
 
-class AM2315BME280MuxSensor(Device):
+class EnvironmentAM2315BME280Sensor(Device):
     CtrlDevice = device_property(
         dtype=str,
-        default_value="domain/family/memeber",
+        default_value="domain/family/member",
         )
 
     SensorType = device_property(
         dtype=str,
         default_value="AM2315",
+        doc="AM2315 or BME280"
         )
+
     Channel = device_property(
         dtype=int,
         default_value=0,
         )
-    @DebugIt()
+
     def init_device(self):
-        
         Device.init_device(self)
         self.set_state(DevState.INIT)
         try:
@@ -37,14 +39,15 @@ class AM2315BME280MuxSensor(Device):
         except AttributeError:
             self.error_stream('Connection could not be established.')
             self.set_state(DevState.OFF)
-            
+
         if self.SensorType.lower() == "am2315":
             self.sens_int = 0
-            self._attr_lib = {"temperature": 0.0, "humidity" : 0.0}
+            self._attr_lib = {"temperature": 0.0, "humidity": 0.0}
             self._address = 0x5c
         elif self.SensorType.lower() == "bme280":
             self.sens_int = 1
-            self._attr_lib = {"temperature": 0.0, "humidity" : 0.0, "pressure" : 0.0}
+            self._attr_lib = {"temperature": 0.0, "humidity": 0.0,
+                              "pressure": 0.0}
             self._address = 0x77
         else:
             self.error_stream('Worng Address')
@@ -52,65 +55,38 @@ class AM2315BME280MuxSensor(Device):
 
         try:
             e = ''
-            e = self.ctrl.init_sensor((self.Channel,self.sens_int))
+            e = self.ctrl.init_sensor((self.Channel, self.sens_int))
         except (AttributeError, DevFailed):
             self.error_stream('Controller not started')
             self.set_state(DevState.OFF)
         if e != '':
             self.error_stream(e)
             self.set_state(DevState.FAULT)
-            
+
         for i in self._attr_lib:
             self.create_attributes(i)
         self.read_out = []
-        
-    @DebugIt()
-    def always_executed_hook(self):
-        pass
-        '''
-        try:
-            # _read_data measures both humidity and temperature
-            self.read_out = self.ctrl.read_data((self.Channel,self.sens_int))
-            self.set_state(DevState.ON)
-            
-        except (AttributeError, DevFailed, ConnectionFailed):
-            self.error_stream('Controller not started')
-            self.set_state(DevState.OFF)
-            return
-        if len(self.read_out) < 2:
-            self.error_stream('Data could not be read')
-            self.set_state(DevState.FAULT)
-            for i,a in enumerate(self._attr_lib):
-                self._attr_lib[a] = float([-1,-1,-1][i])
-        else:
-            for i,a in enumerate(self._attr_lib):
-                    self._attr_lib[a] = float(self.read_out[i])
-                    '''
 
-    @DebugIt()
     def create_attributes(self, argin):
-        
         """
         Command creates a new Attribute
         :param argin: 'DevFloat'
         dev_name
         :return:None
         """
-        attr = attribute(
-            name=argin,
-            dtype=float,
-            access=AttrWriteType.READ,
-            label=argin,
-        ).to_attr()
-        self.add_attribute(attr,r_meth=self.read_value)
-    
-    def read_value(self,attr):
+        attr = attribute(name=argin, dtype=float, access=AttrWriteType.READ,
+                         label=argin,
+                         ).to_attr()
+        self.add_attribute(attr, r_meth=self.read_value)
+
+    def read_value(self, attr):
         if attr.get_name() == "temperature":
             try:
-            # _read_data measures both humidity and temperature
-                self.read_out = self.ctrl.read_data((self.Channel,self.sens_int))
+                # read_data measures both humidity and temperature
+                self.read_out = self.ctrl.read_data((self.Channel,
+                                                     self.sens_int))
                 self.set_state(DevState.ON)
-            
+
             except (AttributeError, DevFailed, ConnectionFailed):
                 self.error_stream('Controller not started')
                 self.set_state(DevState.OFF)
@@ -118,17 +94,16 @@ class AM2315BME280MuxSensor(Device):
             if len(self.read_out) < 2:
                 self.error_stream('Data could not be read')
                 self.set_state(DevState.FAULT)
-                for i,a in enumerate(self._attr_lib):
-                    self._attr_lib[a] = float([-1,-1,-1][i])
+                for i, a in enumerate(self._attr_lib):
+                    self._attr_lib[a] = float([-1, -1, -1][i])
             else:
-                for i,a in enumerate(self._attr_lib):
-                        self._attr_lib[a] = float(self.read_out[i])
-                        
+                for i, a in enumerate(self._attr_lib):
+                    self._attr_lib[a] = float(self.read_out[i])
+
         value = self._attr_lib[attr.get_name()]
         attr.set_value(value)
         return value
-        
 
 
 if __name__ == "__main__":
-    AM2315BME280MuxSensor.run_server()
+    EnvironmentAM2315BME280Sensor.run_server()
